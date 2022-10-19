26f9f091f574e54a646369667a0e8f6d7058a8bd
Nolan Knill <nolanknill@gmail.com>
2022-10-19 13:48:07 -0400

    Callbacks explained

--- scripts/script.js
+++ scripts/script.js
@@ -1,97 +1,34 @@
-/**
- * We need 4 things when creating Event Handlers
- *
- * 1. Element -> document.createElement / querySelector
- * 2. addEventListener
- * 3. type of event -> String
- * 4. function: Event Handler
- */
+/*
+setTimeout(function() {
+  console.log("Hello")
+}, 3000);
 
-document.addEventListener("mousemove", (event) => {
-  console.log(event);
-});
 
-/**
- * Change color of the page__title element when clicked
- * Also, prevent propagation of click events!
- */
-const pageTitleEl = document.querySelector(".page__title");
-pageTitleEl.addEventListener("click", (event) => {
-  // Adding styles to JavaScript breaks Separation of Concerns policy
-  // Do NOT do this:
-  // pageTitleEl.style.color = "red";
+const returnValue = console.log("Hello");
+setTimeout(returnValue, 3000);
+*/
 
-  // add modified class to the element
-  pageTitleEl.classList.add("page__title--clicked");
-
-  // prevent event bubbling
-  event.stopPropagation();
-});
-
-/** Add a click event to the entire page that updates the background color */
-const pageEl = document.querySelector(".page");
-pageEl.addEventListener("click", () => {
-  pageEl.classList.add("page--red");
-});
-
-/**
- * When clicking on a to do item, add a --completed modifier
- * that adds different styles in scss. Use querySelectorAll()
- * to grab all elements as a NodeList, add a loop going from 0 to
- * the length of the to do items. Then grab individual element in each
- * iteration and add a modifier class!
- */
-const todoItemEls = document.querySelectorAll(".todos__item");
-for (let i = 0; i < todoItemEls.length; i++) {
-  const todoItemEl = todoItemEls[i];
-  todoItemEl.addEventListener("click", () => {
-    todoItemEl.classList.toggle("todos__item--completed");
-  });
+function displayGreeting(name) {
+  console.log("Hello, my name is " + name);
 }
 
-/**
- * If the user enters the Konami code on their keyboard, alert
- * a message to the user */
-const konamiCode = [
-  "ArrowUp",
-  "ArrowUp",
-  "ArrowDown",
-  "ArrowDown",
-  "ArrowLeft",
-  "ArrowRight",
-  "ArrowLeft",
-  "ArrowRight",
-  "b",
-  "a",
-];
+setTimeout(function() {
+  displayGreeting("Mahdi");
+}, 2000);
 
-/**
- * When a user enters a key, check if the inputted key is the next key 
- * in the Konami Code sequence. If so, add it to the inputKeys array, 
- * otherwise, reset the sequence by emptying the inputKeys array.
- * Once the input keys matches up with the Konami Code, show Game Over
- * screen.
- */
-let inputKeys = [];
-window.addEventListener("keyup", (event) => {
-  // if the event.key is the right one in sequence of konamiCode, add it to inputKeys
-  if (konamiCode[inputKeys.length] === event.key) {
-    inputKeys.push(event.key);
-  } else {
-    inputKeys = [];
-  }
 
-  if (inputKeys.length === konamiCode.length) {
-      // when the konami code is complete, run the code in 
-      // showGameOverScreen function
-      showGameOverScreen();
-  }
-});
+/* How do we access HTML Elements via the DOM */
 
-/**
- * Hide page element and show the game over element!
- */
-function showGameOverScreen() {
-    document.querySelector(".page").classList.add("page--hidden");
-    document.querySelector(".game-over").classList.add('game-over--show');
-}
+// 1.
+const navItemEl = document.querySelector(".nav__item");
+
+// 2.
+const someIdEl = document.getElementById("some-id")
+
+// 3. <button class="button">Please click me</button>
+const buttonEl = document.createElement("button");
+buttonEl.innerText = "Please click me";
+buttonEl.classList.add("button");
+
+// 4. an array / can't use this
+// document.getElementsByClassName("nav__item");
