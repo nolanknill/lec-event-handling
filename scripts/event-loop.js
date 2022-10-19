b5f9b40c5507fe5ca6914197422367ce00b84d6b
Nolan Knill <nolanknill@gmail.com>
2022-10-19 14:43:43 -0400

    Finished lecture code

---
+++ scripts/event-loop.js
@@ -0,0 +1,17 @@
+setTimeout(function() {
+	console.log("setTimeout");
+}, 0);
+
+for (let i = 0; i < 5; i++) {
+	console.log("1st loop: " + i);
+}
+
+
+for (let i = 0; i < 5; i++) {
+	console.log("2nd loop: " + i);
+}
+
+
+for (let i = 0; i < 5; i++) {
+	console.log("3rd loop: " + i);
+}
--- scripts/script.js
+++ scripts/script.js
@@ -8,6 +8,7 @@ const returnValue = console.log("Hello");
 setTimeout(returnValue, 3000);
 */
 
+/*
 function displayGreeting(name) {
   console.log("Hello, my name is " + name);
 }
@@ -15,20 +16,76 @@ function displayGreeting(name) {
 setTimeout(function() {
   displayGreeting("Mahdi");
 }, 2000);
+*/
+
+/* 
+  How do we access HTML Elements via the DOM
+    1. const navItemEl = document.querySelector(".nav__item");
+    2. const someIdEl = document.getElementById("some-id")
+    3. const buttonEl = document.createElement("button");
+  */
 
 
-/* How do we access HTML Elements via the DOM */
+const todosItemEls = document.querySelectorAll(".todos__item");
 
-// 1.
-const navItemEl = document.querySelector(".nav__item");
+for (let i = 0; i < todosItemEls.length; i++) {
+  const todosItemEl = todosItemEls[i];
 
-// 2.
-const someIdEl = document.getElementById("some-id")
+  todosItemEl.addEventListener("click", function(event) {
+    // Change the CSS properties somehow
+    todosItemEl.classList.toggle("todos__item--completed");
+  });
+}
 
-// 3. <button class="button">Please click me</button>
-const buttonEl = document.createElement("button");
-buttonEl.innerText = "Please click me";
-buttonEl.classList.add("button");
+const pageEl = document.querySelector(".page");
+pageEl.addEventListener("click", function() {
+  pageEl.classList.add("page--clicked");
+})
 
-// 4. an array / can't use this
-// document.getElementsByClassName("nav__item");
+
+
+// window.addEventListener("mousemove", (event) => {
+//   console.log(event);
+// })
+
+
+// window.addEventListener("click", (event) => {
+//   console.log(event);
+// })
+
+
+const secretCode = [
+  "ArrowUp", 
+  "ArrowUp", 
+  "ArrowDown",
+  "ArrowDown",
+  "ArrowLeft", 
+  "ArrowRight", 
+  "ArrowLeft", 
+  "ArrowRight", 
+  "b", 
+  "a"
+];
+
+let inputKeys = [];
+
+window.addEventListener("keyup", (event) => {
+  // Let's check if it is part of the konami code
+  
+  if (event.key === secretCode[inputKeys.length]) {
+    inputKeys.push(event.key);
+  } else {
+    inputKeys = [];
+  }
+  
+  console.log(inputKeys);
+
+  if (inputKeys.length === secretCode.length) {
+    showGameOverScreen();
+  }
+})
+
+function showGameOverScreen() {
+  alert("Game Over");
+}
+
